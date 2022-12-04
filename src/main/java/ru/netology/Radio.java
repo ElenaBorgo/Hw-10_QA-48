193462c4da386ed78d74e534e95ec9c4f9513d05
Elena Borgoiakova <borgoiakova.elen888@gmail.com>
2022-12-04 13:27:12 +0700

    new commit

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,21 +1,55 @@
 package ru.netology;
 
 public class Radio {
+    private int numberOfRadioStations = 10;
+    private int firstRadioStation = 0;
+    private int lastRadioStation = 9;
     private int currentRadioStation;
+
+    private int maxVolume = 100;
+    private int minVolume = 0;
     private int currentVolume;
+
+    public Radio(int numberOfRadioStations) {
+        this.numberOfRadioStations = numberOfRadioStations;
+    }
+
+    public Radio() {
+        this.numberOfRadioStations = getNumberOfRadioStations();
+    }
+
+    public int getNumberOfRadioStations() {
+        return numberOfRadioStations;
+    }
+
+    public int getFirstRadioStation() {
+        return firstRadioStation;
+    }
+
+    public int getLastRadioStation() {
+        return lastRadioStation;
+    }
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
     }
 
     public void setCurrentRadioStation(int newCurrentRadioStation) {
-        if (newCurrentRadioStation < 0) {
-            return;
+        if (newCurrentRadioStation > lastRadioStation) {
+            newCurrentRadioStation = firstRadioStation;
         }
-        if (newCurrentRadioStation > 9) {
-            return;
+        if (newCurrentRadioStation < firstRadioStation) {
+            newCurrentRadioStation = lastRadioStation;
         }
         currentRadioStation = newCurrentRadioStation;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
     }
 
     public int getCurrentVolume() {
@@ -23,36 +57,12 @@ public class Radio {
     }
 
     public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume > 10) {
-            return;
+        if (newCurrentVolume > maxVolume) {
+            newCurrentVolume = maxVolume;
         }
-        if (newCurrentVolume < 0) {
-            return;
+        if (newCurrentVolume < minVolume) {
+            newCurrentVolume = minVolume;
         }
         currentVolume = newCurrentVolume;
-    }
-
-    public void next() {
-        if (currentRadioStation < 9) {
-            currentRadioStation++;
-        } else {
-            currentRadioStation = 0;
-        }
-    }
-
-    public void prev() {
-        if (currentRadioStation > 0) {
-            currentRadioStation = currentRadioStation - 1;
-        } else {
-            currentRadioStation = 9;
-        }
-    }
-
-    public void increaseVolume() {
-        if (currentVolume < 10) {
-            currentVolume++;
-        } else {
-            currentVolume = 10;
-        }
     }
 }
