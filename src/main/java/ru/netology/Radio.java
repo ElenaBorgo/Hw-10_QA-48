0386522887049eddf63fab08b549d11c8910e65b
Elena Borgoiakova <borgoiakova.elen888@gmail.com>
2022-12-04 13:17:00 +0700

    Poshel Nahuy commit

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,13 +1,5 @@
 package ru.netology;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-
 public class Radio {
     private int currentRadioStation;
     private int currentVolume;
